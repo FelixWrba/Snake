866ae9bb9bc47eb2addf3903d3649f205c43f834
Felix Wrba <felix.wrba@web.de>
2024-10-03 11:03:34 +0200

    Added menus and fixed bugs

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ let loopId;
 let speed = 200;
 let direction; // 1 = up, 2 = right, 3 = down, 4 = left
 let snake; // [ { x: int, y: int }, .. ]
+let highscore = +localStorage.getItem('highscore') || 0;
 
 // apple
 let apple = {};
@@ -50,9 +51,12 @@ function loop() {
     if (snake[lastPart].x == apple.x && snake[lastPart].y == apple.y) {
         appleEaten = true;
     }
+    // check if snake collides with tail
+    let snakeBody = snake.slice(0, lastPart);
+    let eatenItself = snakeBody.find(part => part.x == snake[lastPart].x && part.y == snake[lastPart].y);
     // check border collision
-    if(snake[lastPart].x < 0 || snake[lastPart].x >= squares || snake[lastPart].y < 0 || snake[lastPart].y >= squares) {
-        die();
+    if (snake[lastPart].x < 0 || snake[lastPart].x >= squares || snake[lastPart].y < 0 || snake[lastPart].y >= squares || eatenItself) {
+        stopGame('death');
     }
 }
 
@@ -61,29 +65,33 @@ function draw() {
     ctx.fillStyle = 'lightgreen';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'red';
-    ctx.fillRect(apple.x * squareSize, apple.y * squareSize, squareSize, squareSize);
-    ctx.fillStyle = 'darkgreen';
-    snake.forEach(part => ctx.fillRect(part.x * squareSize, part.y * squareSize, squareSize, squareSize));
+    ctx.fillRect(apple.x * squareSize, apple.y * squareSize, squareSize - 1, squareSize - 1);
+    ctx.fillStyle = 'green';
+    snake.forEach(part => ctx.fillRect(part.x * squareSize, part.y * squareSize, squareSize - 1, squareSize - 1));
 
     animationId = requestAnimationFrame(draw);
 }
 
 // handles key presses from event listener
 function keyDown(e) {
-    if (e.keyCode == 38) {
+    if (e.keyCode == 38 && direction != 3) { // up
         direction = 1;
     }
-    if (e.keyCode == 39) {
+    else if (e.keyCode == 39 && direction != 4) { // right
         direction = 2;
     }
-    if (e.keyCode == 40) {
+    else if (e.keyCode == 40 && direction != 1) { // left
         direction = 3;
     }
-    if (e.keyCode == 37) {
+    else if (e.keyCode == 37 && direction != 2) { // down
         direction = 4;
     }
-    if(e.keyCode == 82) {
-        die();
+    if (e.keyCode == 82) {
+        stopGame('death');
+        startGame();
+    }
+    else if(e.keyCode == 80) {
+        stopGame('cancel');
         startGame();
     }
 }
@@ -103,10 +111,41 @@ function startGame() {
     draw();
 }
 
-function die() {
+function mainMenu() {
+    // display main menu
+    ctx.fillStyle = 'lightgreen';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'black';
+    ctx.textAlign = 'center';
+    ctx.font = "60px sans-serif";
+    ctx.fillText('Snake', canvas.width / 2, canvas.height / 3, canvas.width);
+    ctx.font = "20px monospace";
+    ctx.fillText('Press [P] to play', canvas.width / 2, canvas.height / 3 + 90, canvas.width);
+    ctx.font = "20px sans-serif";
+    ctx.textAlign = 'right';
+    ctx.fillText(`Highscore: ${highscore}`, canvas.width - 30, canvas.height - 30, canvas.width);
+}
+
+// display death screen
+function stopGame(page) {
     cancelAnimationFrame(animationId);
     clearInterval(loopId);
-    // display death message
+    if(page == 'death') {
+        deathScreen();
+    }
+    else if(page == 'main'){
+        mainMenu();
+    }
+}
+
+function deathScreen() {
+    // check if new highscore
+    let highscoreText = 'Highscore';
+    if (snake.length > highscore) {
+        highscore = snake.length;
+        localStorage.setItem('highscore', '' + highscore);
+        highscoreText = 'New Highscore!';
+    }
     ctx.fillStyle = 'lightgreen';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'black';
@@ -115,8 +154,10 @@ function die() {
     ctx.fillText('You died.', canvas.width / 2, canvas.height / 3, canvas.width);
     ctx.font = "24px sans-serif";
     ctx.fillText(`Score: ${snake.length}`, canvas.width / 2, canvas.height / 3 + 60, canvas.width);
-    ctx.fillText('Press [R] to restart', canvas.width / 2, canvas.height / 3 + 90, canvas.width);
+    ctx.fillText(`${highscoreText}: ${highscore}`, canvas.width / 2, canvas.height / 3 + 90, canvas.width);
+    ctx.font = "20px monospace";
+    ctx.fillText('Press [R] to restart', canvas.width / 2, canvas.height / 3 + 150, canvas.width);
 }
 
-startGame();
+stopGame('main');
 document.addEventListener('keydown', keyDown);
